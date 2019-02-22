b10435471c81fbf6af0d4511f95eaf23c440cbae
Patrick Olvera <patrickolvera@gmail.com>
2019-02-22 14:36:01 -0600

    feat: Added list of pets and routing

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#Front End Masters: Intro to React v4
+# Front End Masters: Intro to React v4
--- src/app.js
+++ src/app.js
@@ -1,35 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { Router, Link } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title!");
-  }
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Poodle"
-      }),
-      React.createElement(Pet, {
-        name: "Hunter",
-        animal: "dog",
-        breed: "German Shepherd"
-      }),
-      React.createElement(Pet, {
-        name: "Mikey",
-        animal: "cat",
-        breed: "tabby"
-      })
-    ]);
+    return (
+      <div>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    );
   }
 }
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
